9f043f0aeea94cad080a1c2fa038d4f0b7f4262a
Ragats Bálint <82815363+RagiBali1@users.noreply.github.com>
2022-01-12 15:48:41 +0100

    Add files via upload

--- beadando.py
+++ beadando.py
@@ -1,11 +1,30 @@
-import random
-
-darab = [0] * 13
-
-for _ in range(50000):
-    dobas1 = random.randint(1, 6)
-    dobas2 = random.randint(1, 6)
-    darab[dobas1+dobas2] += 1
-
-for dobas in range(len(darab)):
-    print( dobas,": ",darab[dobas],"alkalommal dobtuk.")
+print("Magyarul vagy angolul szeretnel beszelgetni?")
+nyelv = input("Add meg a nyelvet: ")
+if nyelv == "magyar":
+    print("Hello!")
+    nev = input("Add meg  neved kerlek: ")
+    print("Hello!", nev, "Minecraft vagy Warzone?")
+    valasztas1 = input("Válassz!: ")
+    if valasztas1 == "Minecraft":
+        print("Nincs mirol beszelnunk.")
+    if valasztas1 == "Warzone":
+        print("Gyere igyunk mg valamit! De elotte mondd meg az eletkorodat legyszives.")
+        kor = int(input("Add meg eletkorod: "))
+        if kor >= 18:
+            print("Akkor menjun sorozni!")
+        else:
+            print("Akkor ebbol almale lesz!")
+if nyelv == "angol":
+    print("Hi!")
+    nev = input("What's your name?: ")
+    print("Hi!", nev, "Mincraft or Warzone?")
+    valasztas1 = input("Choose!: ")
+    if valasztas1 == "Miinecraft":
+        print("Don't talk with me!")
+    if valasztas1 == "Warzone":
+        print("Come, drink something! But, how old are you?")
+        kor = int(input("Your age: "))
+        if kor >= 18:
+            print("Let's drink a beer!")
+        else:
+            print("You can have only Apple juice! :)")
